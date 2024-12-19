bb2a194e6ef9f4ebf32f0759d13049defcbc3f32
Flan-dre <ptlmc12@gmail.com>
2024-12-19 15:15:48 +0700

    Add files via upload

---
+++ De02/De02/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace De02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvSanpham = new System.Windows.Forms.DataGridView();
+            this.txtmasp = new System.Windows.Forms.TextBox();
+            this.dtngaynhap = new System.Windows.Forms.DateTimePicker();
+            this.cbloaisp = new System.Windows.Forms.ComboBox();
+            this.txttensp = new System.Windows.Forms.TextBox();
+            this.btthem = new System.Windows.Forms.Button();
+            this.btsua = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.btluu = new System.Windows.Forms.Button();
+            this.btkluu = new System.Windows.Forms.Button();
+            this.btthoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.lvSanpham)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvSanpham
+            // 
+            this.lvSanpham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lvSanpham.Location = new System.Drawing.Point(12, 142);
+            this.lvSanpham.Name = "lvSanpham";
+            this.lvSanpham.RowHeadersWidth = 51;
+            this.lvSanpham.RowTemplate.Height = 24;
+            this.lvSanpham.Size = new System.Drawing.Size(562, 243);
+            this.lvSanpham.TabIndex = 0;
+            this.lvSanpham.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.lvSanpham_CellClick);
+            // 
+            // txtmasp
+            // 
+            this.txtmasp.Location = new System.Drawing.Point(726, 142);
+            this.txtmasp.Name = "txtmasp";
+            this.txtmasp.Size = new System.Drawing.Size(100, 22);
+            this.txtmasp.TabIndex = 1;
+            this.txtmasp.TextChanged += new System.EventHandler(this.txtmasp_TextChanged);
+            // 
+            // dtngaynhap
+            // 
+            this.dtngaynhap.Location = new System.Drawing.Point(726, 300);
+            this.dtngaynhap.Name = "dtngaynhap";
+            this.dtngaynhap.Size = new System.Drawing.Size(200, 22);
+            this.dtngaynhap.TabIndex = 3;
+            this.dtngaynhap.ValueChanged += new System.EventHandler(this.dtngaynhap_ValueChanged);
+            // 
+            // cbloaisp
+            // 
+            this.cbloaisp.FormattingEnabled = true;
+            this.cbloaisp.Location = new System.Drawing.Point(726, 361);
+            this.cbloaisp.Name = "cbloaisp";
+            this.cbloaisp.Size = new System.Drawing.Size(121, 24);
+            this.cbloaisp.TabIndex = 4;
+            this.cbloaisp.SelectedIndexChanged += new System.EventHandler(this.cbloaisp_SelectedIndexChanged);
+            // 
+            // txttensp
+            // 
+            this.txttensp.Location = new System.Drawing.Point(726, 221);
+            this.txttensp.Name = "txttensp";
+            this.txttensp.Size = new System.Drawing.Size(100, 22);
+            this.txttensp.TabIndex = 5;
+            this.txttensp.TextChanged += new System.EventHandler(this.txttensp_TextChanged);
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(59, 412);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(75, 23);
+            this.btthem.TabIndex = 6;
+            this.btthem.Text = "Them";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // btsua
+            // 
+            this.btsua.Location = new System.Drawing.Point(152, 412);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(75, 23);
+            this.btsua.TabIndex = 7;
+            this.btsua.Text = "Sua";
+            this.btsua.UseVisualStyleBackColor = true;
+            this.btsua.Click += new System.EventHandler(this.btsua_Click);
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(242, 412);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(75, 23);
+            this.btxoa.TabIndex = 8;
+            this.btxoa.Text = "Xoa";
+            this.btxoa.UseVisualStyleBackColor = true;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
+            // 
+            // btluu
+            // 
+            this.btluu.Location = new System.Drawing.Point(347, 411);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(75, 23);
+            this.btluu.TabIndex = 9;
+            this.btluu.Text = "Luu";
+            this.btluu.UseVisualStyleBackColor = true;
+            this.btluu.Click += new System.EventHandler(this.btluu_Click);
+            // 
+            // btkluu
+            // 
+            this.btkluu.Location = new System.Drawing.Point(453, 412);
+            this.btkluu.Name = "btkluu";
+            this.btkluu.Size = new System.Drawing.Size(75, 23);
+            this.btkluu.TabIndex = 10;
+            this.btkluu.Text = "K.luu";
+            this.btkluu.UseVisualStyleBackColor = true;
+            this.btkluu.Click += new System.EventHandler(this.btkluu_Click);
+            // 
+            // btthoat
+            // 
+            this.btthoat.Location = new System.Drawing.Point(557, 412);
+            this.btthoat.Name = "btthoat";
+            this.btthoat.Size = new System.Drawing.Size(75, 23);
+            this.btthoat.TabIndex = 11;
+            this.btthoat.Text = "Thoat";
+            this.btthoat.UseVisualStyleBackColor = true;
+            this.btthoat.Click += new System.EventHandler(this.btthoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(640, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ma SP :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(640, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Ten SP :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(640, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "NgayNhap:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(649, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "LoaiSP :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 550);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btthoat);
+            this.Controls.Add(this.btkluu);
+            this.Controls.Add(this.btluu);
+            this.Controls.Add(this.btxoa);
+            this.Controls.Add(this.btsua);
+            this.Controls.Add(this.btthem);
+            this.Controls.Add(this.txttensp);
+            this.Controls.Add(this.cbloaisp);
+            this.Controls.Add(this.dtngaynhap);
+            this.Controls.Add(this.txtmasp);
+            this.Controls.Add(this.lvSanpham);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lvSanpham)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView lvSanpham;
+        private System.Windows.Forms.TextBox txtmasp;
+        private System.Windows.Forms.DateTimePicker dtngaynhap;
+        private System.Windows.Forms.ComboBox cbloaisp;
+        private System.Windows.Forms.TextBox txttensp;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Button btkluu;
+        private System.Windows.Forms.Button btthoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ De02/De02/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+
+
+
+namespace De02
+{
+    public partial class Form1 : Form
+    {
+        // Chuỗi kết nối đến cơ sở dữ liệu
+        private string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=QLSanpham1;Integrated Security=True";
+
+
+        // Khởi tạo SqlConnection
+        private SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(connectionString);
+            LoadLoaiSP(); // Tải dữ liệu LoaiSP khi khởi động form
+            LoadSanPham(); // Tải dữ liệu Sanpham khi khởi động form
+            string queryLoaiSP = "SELECT * FROM LoaiSP";
+            string querySanpham = "SELECT * FROM Sanpham";
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("vi-VN");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("vi-VN");
+            
+
+            
+
+        }
+
+        private bool isEditing = false;
+
+        private void LoadLoaiSP()
+        {
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM LoaiSP";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    cbloaisp.Items.Add(new KeyValuePair<string, string>(
+                        reader["MaLoai"].ToString(),
+                        reader["TenLoai"].ToString()));
+                }
+           
+                cbloaisp.DisplayMember = "Value";
+                cbloaisp.ValueMember = "Key";
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void LoadSanPham()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Sanpham";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    lvSanpham.DataSource = table; // Gán dữ liệu cho DataGridView
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+
+
+        private void txtmasp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txttensp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbloaisp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbloaisp.SelectedItem != null)
+            {
+                var selectedPair = (KeyValuePair<string, string>)cbloaisp.SelectedItem;
+                MessageBox.Show($"Mã Loại: {selectedPair.Key}, Tên Loại: {selectedPair.Value}");
+            }
+        }
+
+        private void btthoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+       "Bạn có chắc chắn muốn thoát không?",
+       "Xác nhận thoát",
+       MessageBoxButtons.YesNo,
+       MessageBoxIcon.Question
+   );
+
+            if (result == DialogResult.Yes)
+            {
+                this.Close(); // Thoát ứng dụng
+            }
+        }
+
+        private void btkluu_Click(object sender, EventArgs e)
+        {
+
+            isEditing = false;
+            UpdateButtonState();
+
+            // Khôi phục dữ liệu ban đầu
+          
+
+            MessageBox.Show("Các thay đổi đã bị hủy.");
+        }
+
+        private void UpdateButtonVisibility()
+        {
+            btluu.Visible = isEditing;   // Hiển thị nút Lưu nếu đang Thêm/Sửa/Xóa
+            btkluu.Visible = isEditing; // Hiển thị nút Không lưu nếu đang Thêm/Sửa/Xóa
+        }
+
+        private void UpdateButtonState()
+        {
+            btluu.Enabled = isEditing;   // Chỉ cho phép nhấn nút Lưu khi đang Thêm/Xóa/Sửa
+            btkluu.Enabled = isEditing; // Chỉ cho phép nhấn nút Không Lưu khi đang Thêm/Xóa/Sửa
+        }
+
+        
+
+        private void btluu_Click(object sender, EventArgs e)
+        {
+            isEditing = false;
+            UpdateButtonState();
+
+            MessageBox.Show("Dữ liệu đã được lưu vào cơ sở dữ liệu.");
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Bạn có chắc chắn muốn xóa sản phẩm này?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Sanpham WHERE MaSP = @MaSP";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@MaSP", txtmasp.Text);
+
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Xóa sản phẩm thành công!");
+                    LoadSanPham();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void btsua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cbloaisp.SelectedItem == null)
+                {
+                    MessageBox.Show("Vui lòng chọn một loại sản phẩm!");
+                    return;
+                }
+
+                connection.Open();
+                string query = "UPDATE Sanpham SET TenSP = @TenSP, MaLoai = @MaLoai WHERE MaSP = @MaSP";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaSP", txtmasp.Text);
+                command.Parameters.AddWithValue("@TenSP", txttensp.Text);
+                command.Parameters.AddWithValue("@MaLoai", ((KeyValuePair<string, string>)cbloaisp.SelectedItem).Key);
+                command.Parameters.AddWithValue("@NgayNhap", dtngaynhap.Value); // Giá trị từ DateTimePicker
+
+                command.ExecuteNonQuery();
+                MessageBox.Show("Sửa sản phẩm thành công!");
+                LoadSanPham();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                string query = "INSERT INTO Sanpham (MaSP, TenSP, NgayNhap, MaLoai) VALUES (@MaSP, @TenSP, @NgayNhap, @MaLoai)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaSP", txtmasp.Text);
+                command.Parameters.AddWithValue("@TenSP", txttensp.Text);
+                command.Parameters.AddWithValue("@NgayNhap", DateTime.Now); // Hoặc lấy từ DateTimePicker
+                command.Parameters.AddWithValue("@MaLoai", ((KeyValuePair<string, string>)cbloaisp.SelectedItem).Key);
+
+                command.ExecuteNonQuery();
+                MessageBox.Show("Thêm sản phẩm thành công!");
+                LoadSanPham();
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            isEditing = true;
+            UpdateButtonState(); // Kích hoạt nút Lưu và Không Lưu
+            txtmasp.Text = "";
+            txttensp.Text = "";
+            cbloaisp.SelectedIndex = -1;
+            dtngaynhap.Value = DateTime.Now;
+        }
+
+        private void dtngaynhap_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void lvSanpham_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = lvSanpham.Rows[e.RowIndex];
+
+                txtmasp.Text = row.Cells["MaSP"].Value.ToString();
+                txttensp.Text = row.Cells["TenSP"].Value.ToString();
+                cbloaisp.SelectedValue = row.Cells["MaLoai"].Value.ToString();
+
+                // Hiển thị ngày nhập
+                if (row.Cells["NgayNhap"].Value != DBNull.Value)
+                {
+                    dtngaynhap.Value = Convert.ToDateTime(row.Cells["NgayNhap"].Value);
+                }
+                else
+                {
+                    dtngaynhap.Value = DateTime.Now; // Mặc định ngày hiện tại
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
